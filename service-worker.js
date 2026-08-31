63204683e482ad71be221d61814acc91c7294845
Pavel <pavel@local>
2026-08-31 12:40:33 +0200

    Zpresnit a vizualne vyladit landing page

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
  * Provides offline caching with stale-while-revalidate strategy
  */
 
-const CACHE_NAME = 'mysticka-hvezda-c84efffb120f';
+const CACHE_NAME = 'mysticka-hvezda-9fbbfbbc98f4';
 const MAX_RUNTIME_CACHE_SIZE = 150;
 const STATIC_ASSETS = [
     '/fonts/local-fonts.css',
